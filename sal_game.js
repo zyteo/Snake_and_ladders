281e2bc5c458895e486e00590d76d5d11278d29e
Stabilo <zteo015@e.ntu.edu.sg>
2021-09-11 23:13:19 +0800

    added stuff

--- sal_game.js
+++ sal_game.js
@@ -1,2 +1,14 @@
 // GA SEI-32 Project 1: Game
 // ZY, 10 Sept 2021
+
+// FUNCTION Dice roll from 1 to 6
+const diceRoll = () => {
+    let randomDice = Math.ceil(Math.random() * 6);
+    return randomDice;
+}
+
+// ARRAY of snakes
+const snakeTiles = [41,44,49,52,58,62,69,73,84,92,95,99];
+// ARRAY of ladders
+const ladderTiles = [12,51,57,76,78];
+

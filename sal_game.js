3b4a612bd4dd38bf6c5df9c8490339108b82dee1
Stabilo <zteo015@e.ntu.edu.sg>
2021-09-21 14:18:40 +0800

    edited js code, removed snake/ladder array and combined to single object

--- sal_game.js
+++ sal_game.js
@@ -30,209 +30,198 @@ let roundNumber = 1;
 // refer https://stackoverflow.com/questions/26313066/setinterval-and-clearinterval-inside-of-a-button
 let playGame;
 
-// ARRAY of snakes, with resulting tiles that piece will move down to
-const snakeTiles = [41,44,49,58,62,69,73,84,92,95,99];
-const snakeTilesResult = [22,37,34,42,60,50,35,66,90,74,7];
-// ARRAY of ladders, with resulting tiles that piece will move up to
-const ladderTiles = [12,51,57,76,78];
-const ladderTilesResult = [31,70,63,89,96];
+// set the value of the last tile and condition where piece is nearing the last tile
+const lastTile = 100;
+const nearLastTile = 93;
+
+// OBJECT of snakes and ladders, with the resulting tiles that the pieces will move to
+const snakeAndLadders = {
+  start: [41, 44, 49, 58, 62, 69, 73, 84, 92, 95, 99, 12, 51, 57, 76, 78],
+  end: [22, 37, 34, 42, 60, 50, 35, 66, 90, 74, 7, 31, 70, 63, 89, 96],
+};
 
 // FUNCTION Dice roll from 1 to 6
 // Function randomly generates and returns a number, randomDice, that has value 1-6.
 const diceRoll = () => {
-    let randomDice = Math.ceil(Math.random() * 6);
-    return randomDice;
-}
-// FUNCTION Move piece backward if land on snakes
-// Takes in a parameter (piece) that is part of the snakeTiles value
-// Finds that value in snakeTiles array, get the index, and input that index into snakeTilesResult.
-// Function returns a number, resultSnake, that the piece will move to.
-const moveBack = (piece) => {
-    let indexSnake = snakeTiles.indexOf(piece);
-    let resultSnake = snakeTilesResult[indexSnake];
-    return resultSnake;
-}
-// FUNCTION Move piece forward if land on ladder
-// Takes in a parameter (piece) that is part of the ladderTiles value
-// Finds that value in ladderTiles array, get the index, and input that index into ladderTilesResult.
-// Function returns a number, resultLadder, that the piece will move to.
-const moveForward = (piece) => {
-    let indexLadder = ladderTiles.indexOf(piece);
-    let resultLadder = ladderTilesResult[indexLadder];
-    return resultLadder;
-}
+  let randomDice = Math.ceil(Math.random() * 6);
+  return randomDice;
+};
+// FUNCTION Move piece if land on snakes or ladder
+// Takes in a parameter (piece) that is part of the snakeAndLadders object array
+// Finds the corresponding end value, get the index, and input that index into snakeAndLadders.end.
+// Function returns a number, resultSnakeOrLadder, that the piece will move to.
+const moveSnakeOrLadder = (piece) => {
+  let indexSnakeOrLadder = snakeAndLadders.start.indexOf(piece);
+  let resultSnakeOrLadder = snakeAndLadders.end[indexSnakeOrLadder];
+  return resultSnakeOrLadder;
+};
 // FUNCTION Move tile normal
 // Takes in corresponding player's tile number (currentTile), and returns the resulting tile they land on.
 const moveTileNormal = (currentTile) => {
-    let tileAfterRoll = currentTile + diceRoll();
-    //   save the dice value
-    let diceValue = tileAfterRoll - currentTile;
-    // display the dice value on screen
-    $('.screen')[0].textContent = diceValue;
-    // if land on snaketile, return value of tile player goes down to
-    if (snakeTiles.includes(tileAfterRoll)){
-        return moveBack(tileAfterRoll);
-    }
-    // if land on laddertile, return value of tile player goes up to
-    else if (ladderTiles.includes(tileAfterRoll)){
-        return moveForward(tileAfterRoll);
-    }
-    // otherwise, land on normal tile
-    else {
-        return tileAfterRoll;
-    }
-}
+  let tileAfterRoll = currentTile + diceRoll();
+  //   save the dice value
+  let diceValue = tileAfterRoll - currentTile;
+  // display the dice value on screen
+  $(".screen")[0].textContent = diceValue;
+  // if land on snake or ladder, return value of tile player goes to
+  if (snakeAndLadders.start.includes(tileAfterRoll)) {
+    return moveSnakeOrLadder(tileAfterRoll);
+  }
+  // otherwise, land on normal tile
+  else {
+    return tileAfterRoll;
+  }
+};
 // FUNCTION Move tile towards end game
-// Takes in corresponding player's tile number (currentTile), and returns the resulting tile they land on. If exceed tile 100, no movement. 
+// Takes in corresponding player's tile number (currentTile), and returns the resulting tile they land on. If exceed tile 100, no movement.
 const moveTileEnding = (currentTile) => {
-    let tileAfterRoll = currentTile + diceRoll();
-    let diceValue = tileAfterRoll - currentTile;
-    $('.screen')[0].textContent = diceValue;
-    // FIRST CHECK if landed on snake
-    if (snakeTiles.includes(tileAfterRoll)){
-        return moveBack(tileAfterRoll);
-    }
-    //END CHECK, if didn't land on snake, continue on
-    // if less than 100, 
-    else if (tileAfterRoll < 100){
-        return tileAfterRoll;
-    }
-    // If land nicely on last tile, 100.
-    else if (tileAfterRoll === 100){
-        alert('A player has reached 100! Select "New game" to start again!');
-        clearInterval(playGame);
-        return tileAfterRoll;
-    }
-    // if goes beyond last tile, remain in original position
-    else if (tileAfterRoll > 100){
-        return (tileAfterRoll - diceValue);
-    }
-}
+  let tileAfterRoll = currentTile + diceRoll();
+  let diceValue = tileAfterRoll - currentTile;
+  $(".screen")[0].textContent = diceValue;
+  // FIRST CHECK if landed on snake
+  if (snakeAndLadders.start.includes(tileAfterRoll)) {
+    return moveSnakeOrLadder(tileAfterRoll);
+  }
+  //END CHECK, if didn't land on snake, continue on
+  // if less than 100,
+  else if (tileAfterRoll < lastTile) {
+    return tileAfterRoll;
+  }
+  // If land nicely on last tile, 100.
+  else if (tileAfterRoll === lastTile) {
+    alert('A player has reached 100! Select "New game" to start again!');
+    clearInterval(playGame);
+    return tileAfterRoll;
+  }
+  // if goes beyond last tile, remain in original position
+  else if (tileAfterRoll > lastTile) {
+    return tileAfterRoll - diceValue;
+  }
+};
 // FUNCTION Make table row + table details
 // Takes in 2 parameters (start,end) that is the start and end of the squares
 // Generates a row of 10 squares, with respective id
-const make1Row = (start,end) => {
-    const $newrow = $('<tr>').addClass('tablerow');
-    // if start < end, LEFT TO RIGHT
-    if (start < end) {
-        // add the squares inside new row
-        for (let i = start; i<= end; i++){
-            // create individual square
-            const $square = $('<td>').attr('id', i).addClass('square').text(i);
-            // append to table row
-            $newrow.append($square);
-        }
+const make1Row = (start, end) => {
+  const $newrow = $("<tr>").addClass("tablerow");
+  // if start < end, LEFT TO RIGHT
+  if (start < end) {
+    // add the squares inside new row
+    for (let i = start; i <= end; i++) {
+      // create individual square
+      const $square = $("<td>").attr("id", i).addClass("square").text(i);
+      // append to table row
+      $newrow.append($square);
     }
-    // if end < start, RIGHT TO LEFT
-    else if (end < start) {
-        for (let i = start; i>= end; i--){
-            const $square = $('<td>').attr('id', i).addClass('square').text(i);
-            $newrow.append($square);
-        }
+  }
+  // if end < start, RIGHT TO LEFT
+  else if (end < start) {
+    for (let i = start; i >= end; i--) {
+      const $square = $("<td>").attr("id", i).addClass("square").text(i);
+      $newrow.append($square);
     }
-    // append table row to table
-    $('table').append($newrow);
-}
+  }
+  // append table row to table
+  $("table").append($newrow);
+};
 
 // Make the 10x10 board
-make1Row(100,91);
-make1Row(81,90);
-make1Row(80,71);
-make1Row(61,70);
-make1Row(60,51);
-make1Row(41,50);
-make1Row(40,31);
-make1Row(21,30);
-make1Row(20,11);
-make1Row(1,10);
+make1Row(100, 91);
+make1Row(81, 90);
+make1Row(80, 71);
+make1Row(61, 70);
+make1Row(60, 51);
+make1Row(41, 50);
+make1Row(40, 31);
+make1Row(21, 30);
+make1Row(20, 11);
+make1Row(1, 10);
 
-// FUNCTION to progress the game 
+// FUNCTION to progress the game
 // Function will check for round number, and then perform playing options on the pieces. Also updates the text content (round number, player tile and player piece) on the screens.
 const progressGame = () => {
-    // PLAYER 1 ROUND if roundnumber is odd
-    if (roundNumber % 2 !== 0){
-        console.log(`${roundNumber} so player 1!`)
-        // update round number first
-        roundNumber += 1;
-        $('.roundnumber')[0].textContent = roundNumber;
-        // check, if tile greater than 93?
-        if (p1Tile > 93) {
-            p1Tile = moveTileEnding(p1Tile)
-            console.log("endgame p1", p1Tile)
-            $('.p1tile')[0].textContent = p1Tile;
-            // update tile piece
-            $('#' + `${p1Tile}`).append($('.circle1'))
-        }
-        // If tile less than 94, NORMAL 
-        else {
-            p1Tile = moveTileNormal(p1Tile);
-            console.log("p1",p1Tile);
-            $('.p1tile')[0].textContent = p1Tile;
-            $('#' + `${p1Tile}`).append($('.circle1'))
-        }
+  // PLAYER 1 ROUND if roundnumber is odd
+  if (roundNumber % 2 !== 0) {
+    console.log(`${roundNumber} so player 1!`);
+    // update round number first
+    roundNumber += 1;
+    $(".roundnumber")[0].textContent = roundNumber;
+    // check, if tile greater than 93?
+    if (p1Tile > nearLastTile) {
+      p1Tile = moveTileEnding(p1Tile);
+      console.log("endgame p1", p1Tile);
+      $(".p1tile")[0].textContent = p1Tile;
+      // update tile piece
+      $("#" + `${p1Tile}`).append($(".circle1"));
     }
-    
-    // PLAYER 2 ROUND if roundnumber is even
-    else if (roundNumber % 2 === 0){
-        console.log(`${roundNumber} so player 2!`)
-        // update round number first
-        roundNumber += 1;
-        $('.roundnumber')[0].textContent = roundNumber;
-        // check, if tile greater than 93?
-        if (p2Tile > 93) {
-            p2Tile = moveTileEnding(p2Tile);
-            console.log("endgame p2", p2Tile);
-            $('.p2tile')[0].textContent = p2Tile;
-            $('#' + `${p2Tile}`).append($('.circle2'))
-        }
-        // If tile less than 94, NORMAL 
-        else {
-            p2Tile = moveTileNormal(p2Tile);
-            console.log("p2",p2Tile);
-            $('.p2tile')[0].textContent = p2Tile;
-            $('#' + `${p2Tile}`).append($('.circle2'))
-        }
+    // If tile less than 94, NORMAL
+    else {
+      p1Tile = moveTileNormal(p1Tile);
+      console.log("p1", p1Tile);
+      $(".p1tile")[0].textContent = p1Tile;
+      $("#" + `${p1Tile}`).append($(".circle1"));
     }
-}
+  }
+
+  // PLAYER 2 ROUND if roundnumber is even
+  else if (roundNumber % 2 === 0) {
+    console.log(`${roundNumber} so player 2!`);
+    // update round number first
+    roundNumber += 1;
+    $(".roundnumber")[0].textContent = roundNumber;
+    // check, if tile greater than 93?
+    if (p2Tile > nearLastTile) {
+      p2Tile = moveTileEnding(p2Tile);
+      console.log("endgame p2", p2Tile);
+      $(".p2tile")[0].textContent = p2Tile;
+      $("#" + `${p2Tile}`).append($(".circle2"));
+    }
+    // If tile less than 94, NORMAL
+    else {
+      p2Tile = moveTileNormal(p2Tile);
+      console.log("p2", p2Tile);
+      $(".p2tile")[0].textContent = p2Tile;
+      $("#" + `${p2Tile}`).append($(".circle2"));
+    }
+  }
+};
 
 // FUNCTION to progress the game automatically
 // Function uses the built-in setinterval function to run the progressGame function above, interval of 1sec.
 // This is for the "Autoplay" button
 const autoPlay = () => {
-    playGame = setInterval(progressGame, 1000);
-}
+  playGame = setInterval(progressGame, 1000);
+};
 
 // FUNCTION to progress the game
 // Function first clears the setinterval function to stop autoplay and then run the progressGame function.
 // This is for the "Let's roll!" button
 const normalRoll = () => {
-    clearInterval(playGame);
-    progressGame();
-}
+  clearInterval(playGame);
+  progressGame();
+};
 
 // FUNCTION to set dark mode
 // Function checks if checkbox is checked, if yes then enable dark mode, otherwise is light mode
 // This is for the "Dark mode" checkbox
 // refer https://stackoverflow.com/questions/6358673/javascript-checkbox-onchange
 const darkMode = () => {
-    if (document.getElementById('checkbox').checked) {
-        $('body,p')[0].className = "darkmode";
-        $('h2')[0].className = "darkmode";
-        // change the screen colours displaying the dice value etc
-        $('.badge-light').addClass('badge-dark').removeClass('badge-light');
-    } else {
-        $('body,p')[0].className = "";
-        $('h2')[0].className = "";
-        $('.badge-dark').addClass('badge-light').removeClass('badge-dark')
-    }
-}
+  if (document.getElementById("checkbox").checked) {
+    $("body,p")[0].className = "darkmode";
+    $("h2")[0].className = "darkmode";
+    // change the screen colours displaying the dice value etc
+    $(".badge-light").addClass("badge-dark").removeClass("badge-light");
+  } else {
+    $("body,p")[0].className = "";
+    $("h2")[0].className = "";
+    $(".badge-dark").addClass("badge-light").removeClass("badge-dark");
+  }
+};
 
 // jquery/js event listeners
 $(() => {
-
-// on click, reset game by refreshing page
+  // on click, reset game by refreshing page
   $(".refresh").on("click", () => {
     window.location.reload();
-  })
     
-})
+  });
+});
